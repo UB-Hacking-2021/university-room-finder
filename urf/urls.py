ff284e37ec965b3390781b8967f7296016330ee7
Nicholas Myers <32116122+NicholasMy@users.noreply.github.com>
2021-11-06 23:11:06 -0400

    Add debugging and results endpoint

--- urf/urls.py
+++ urf/urls.py
@@ -3,5 +3,7 @@ from urf import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('regenerate', views.regenerate, name="regenerate")
+    path('regenerate', views.regenerate, name="regenerate"),
+    path('results', views.results, name="results"),
+    path('debug', views.debug, name="debug"),
 ]
--- urf/views.py
+++ urf/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from urf import regeneration
+from .models import Course, Room
 
 
 def index(request):
     return render(request, "urf/index.html")
+
+
+def results(request):
+    # TODO give the results
+    data = {"test": "test string"}
+    return render(request, "urf/index.html", {"data": data})
 
 
 # Wipe the database and regenerate all course schedules and rooms
@@ -11,3 +18,12 @@ def regenerate(request):
     regeneration.wipe_database()
     regeneration.build_database()
     return render(request, "urf/regeneration_complete.html")
+
+
+# Wipe the database and regenerate all course schedules and rooms
+def debug(request):
+    data = {
+        "rooms": Room.objects.all(),
+        "courses": Course.objects.all(),
+    }
+    return render(request, "urf/debug.html", {"data": data})
